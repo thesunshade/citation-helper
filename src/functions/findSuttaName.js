f87c1f2123edbabca8dcf22d741195801657db9d
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-09-06 07:17:52 +0530

    fix DPR Dhammapada error

--- src/functions/findSuttaName.js
+++ src/functions/findSuttaName.js
@@ -5,9 +5,7 @@ export default function findSuttaName(bookName, suttaNumber) {
   const secondNumber = suttaNumber.secondNumber;
   let suttaName = "";
 
-  // if (firstNumber === 0) {
-  //   return null;
-  // }
+
   const completeCitation = `${bookName}${firstNumber ? " " + firstNumber : ""}${
     secondNumber ? "." + secondNumber : ""
   }`;
@@ -15,36 +13,11 @@ export default function findSuttaName(bookName, suttaNumber) {
   for (let i = 0; i < suttas.length; i++) {
     if (completeCitation === suttas[i][0].toLowerCase()) {
       suttaName = suttas[i][1];
-      // document.title = `${decodeURI(document.location.search).replace("?q=", "")} ${suttas[i][1]}`;
       i = suttas.length + 1;
     }
   }
 
-  // if the sutta name doesn't end with "Sutta$", "gāthā$", "vatthu$"
-  // then add Sutta to the end
 
-  // if (
-  //   suttaName &&
-  //   !suttaName.match(/Sutta$/) &&
-  //   !suttaName.match(/vatthu$/) &&
-  //   !suttaName.match(/gāthā$/) &&
-  //   !suttaName.match(/khandhaka$/) &&
-  //   !suttaName.match(/Saṁyutta$/) &&
-  //   !suttaName.match(/Nikāya$/) &&
-  //   !suttaName.match(/Khuddakapāṭha$/) &&
-  //   !suttaName.match(/Dhammapada$/) &&
-  //   !suttaName.match(/Udāna$/) &&
-  //   !suttaName.match(/Itivuttaka$/) &&
-  //   !suttaName.match(/Sutta Nipāta$/) &&
-  //   !suttaName.match(/Vimānavatthu$/) &&
-  //   !suttaName.match(/Petavatthu$/) &&
-  //   !suttaName.match(/Theragātha$/) &&
-  //   !suttaName.match(/Therīgāthā$/) &&
-  //   !suttaName.match(/Jātaka$/) &&
-  //   !suttaName.match(/sikkhāpada$/)
-  // ) {
-  //   suttaName += " Sutta";
-  // }
 
   return suttaName;
 }
